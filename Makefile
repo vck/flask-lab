a0fad80fe6b6353e629e12759e78268908b791c3
dasta <vickydasta@gmail.com>
2020-12-03 14:58:18 +0700

    update job, add rule to start rg

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+install:
+	pip3 install -r requirements.txt
+
+start-rq:
+	rq worker --with-scheduler
+
 all:
 	FLASK_APP=app.py flask run
+
--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import time
 import redis
 from flask import Flask, request
 from rq import Queue
+from rq.job import Job
 from job import background_task
+
 
 app = Flask(__name__)
 
@@ -12,6 +14,20 @@ q = Queue(connection=r)
 @app.route('/')
 def index_page():
     return "hello world! visit <a href='/task?n=1'>task</a>"
+
+
+@app.route("/results/<job_key>", methods=["GET"])
+def get_result(job_key):
+    try:
+        job = Job.fetch(job_key, connection=r)
+    except:
+        return "Job not found", 202
+
+    print(job)
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Not finished", 202
 
 
 @app.route('/task')
